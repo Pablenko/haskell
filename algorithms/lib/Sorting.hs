810ed718fc86f9202fc358cd281c91135a030359
Pawel Przybylski <pawel.przybylski@codetain.com>
2023-05-11 21:17:49 +0200

    added merge sorting

--- algorithms/lib/Sorting.hs
+++ algorithms/lib/Sorting.hs
@@ -1,4 +1,4 @@
-module Sorting (quicksort) where
+module Sorting (quicksort, mergesort) where
 
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
@@ -6,3 +6,19 @@ quicksort (x:xs) =
     let left_sorted = quicksort [elem | elem <- xs, elem <= x]
         right_sorted = quicksort [elem | elem <- xs, elem > x]
     in left_sorted ++ [x] ++ right_sorted
+
+
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge [] right           = right
+merge left []            = left
+merge (x:left) (y:right) | x < y     = x:merge left (y:right)
+                         | otherwise = y:merge (x:left) right
+
+
+mergesort :: (Ord a) => [a] -> [a]
+mergesort xs
+    | length xs < 2 = xs
+    | otherwise = merge (mergesort left) (mergesort right)
+    where left = take half xs
+          right = drop half xs
+          half = length xs `div` 2
--- algorithms/test/SortingTests.hs
+++ algorithms/test/SortingTests.hs
@@ -6,13 +6,17 @@ import qualified System.Exit as Exit
 test_quicksort_one_elem_list :: Test
 test_quicksort_one_elem_list = TestCase (assertEqual "quicksort 1 elem" [1] (quicksort [1]))
 
-test_quicksort_list :: Test
-test_quicksort_list = TestCase (assertEqual "quicksort list" [-12, -5, -2, 0, 1, 1, 10] (quicksort [-2, 1, -5, 10, -12, 1, 0]))
+test_quicksort :: Test
+test_quicksort = TestCase (assertEqual "quicksort list" [-12, -5, -2, 0, 1, 1, 10] (quicksort [-2, 1, -5, 10, -12, 1, 0]))
+
+test_mergesort :: Test
+test_mergesort = TestCase (assertEqual "mergesort list" [-12, -10, 1, 10, 12] (mergesort [-10, 10, -12, 12, 1]))
  
 tests :: Test
 tests = TestList [
     TestLabel "test_quicksort_one_elem_list" test_quicksort_one_elem_list,
-    TestLabel "test_quicksort_list" test_quicksort_list
+    TestLabel "test_quicksort" test_quicksort,
+    TestLabel "test_mergesort" test_mergesort
     ]
  
 main :: IO ()
