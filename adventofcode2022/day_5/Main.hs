a2a9c9bc42e769dfc5cbdd99cb9c01b4f5be6308
Pawel Przybylski <pawel.przybylski@codetain.com>
2023-05-30 12:51:50 +0200

    Day 5 work on

--- adventofcode2022/day_5/Main.hs
+++ adventofcode2022/day_5/Main.hs
@@ -5,6 +5,8 @@ import Text.Megaparsec (sepEndBy1, sepBy, sepBy1)
 import Text.Megaparsec.Char (char, eol, string, letterChar, digitChar, hspace)
 import RIO
 import Control.Monad (void)
+import Data.Map (Map, empty, insertWith)
+import Data.Maybe (catMaybes)
 
 
 type StackLine = [Maybe Char]
@@ -24,6 +26,9 @@ data DataInput = DataInput
         indexes :: [Int],
         moves :: [Move]
     } deriving (Show) 
+
+
+type CrateStack = Map Int [Char]
 
 
 crateParser :: Parser (Maybe Char)
@@ -72,8 +77,20 @@ readInput :: (HasLogFunc env) => RIO env DataInput
 readInput = readAndParseInput "day_5/input.txt" dataParser
 
 
-solution :: DataInput-> DataInput
-solution xs = xs
+firstTup :: (a, b) -> a
+firstTup (a, b) = a
+
+
+secondTup :: (a, b) -> b
+secondTup (a, b) = b
+
+
+buildCreateStack :: StackLines -> CrateStack
+buildCreateStack slines = foldl (\stackMap elem -> insertWith (++) (firstTup elem) (catMaybes $ secondTup elem) stackMap) empty $ zip [1..] slines
+
+
+solution :: DataInput -> CrateStack
+solution input = buildCreateStack $ stackLines input
 
 
 run :: (HasLogFunc env) => RIO env ()
